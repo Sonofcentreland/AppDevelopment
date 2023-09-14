bb049fb9fa5801d1b517091295a6bc905a48ca4a
Sonofcentreland <iamjeeva2003@gmail.com>
2023-09-14 15:57:53 +0530

    day 02

---
+++ triwizardrecipehub/src/Components/Login/Login.js
@@ -0,0 +1,29 @@
+function Login() {
+    return (
+      <div className="LoginBorder">
+        <form >
+            <input 
+              type="text" 
+              name="username" 
+              id="username"
+              required
+            />
+            <input 
+              type="password" 
+              name="password" 
+              id="password"
+              required
+            />
+            <input 
+              type="text" 
+              name="username" 
+              id="username"
+              required
+            />
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
